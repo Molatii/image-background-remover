5a8356ae0979ca6ff80184bb7b989daaaece2231
Molati <86184198+Molati@users.noreply.github.com>
2023-12-14 16:54:52 -0800

    Update appRender.test.js

--- src/__test__/appRender.test.js
+++ src/__test__/appRender.test.js
@@ -4,7 +4,6 @@ import Hero from "../components/hero";
 
 test("renders the app correctly", () => {
   render(<Hero />);
-
   // Check if the text "Simplicity Redefined" is in the document
   const linkElement = screen.getByText(/Simplicity Redefined/i);
   expect(linkElement).toBeInTheDocument();
